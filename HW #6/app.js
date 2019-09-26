c26acef4240b37de9934dd109c003fc97a8f0e74
Rob Grande <robert.grande@sitechhs.com>
2019-09-25 22:47:12 -0400

    hw 6

---
+++ HW #6/app.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// math objects
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2,33,5,6,78,2,1);
+val = Math.max(2,33,5,6,78,2,1);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20);
+
+console.log(val);
+
