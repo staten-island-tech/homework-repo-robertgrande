917ca7deafeb7c5edb76a61163e2bf8c7b52c6e1
Rob Grande <robert.grande@sitechhs.com>
2019-10-03 11:09:59 -0400

    hw6adam

--- HW #6/app.js
+++ HW #6/app.js
@@ -1,2 +1,28 @@
-Array.from(document.querySelector('.list'));
+// Array.from(document.querySelector('.list'));
+//you need .children to call the items from the list
 
+let htmlArr = Array.from(document.querySelector('ul').children)
+
+let filteredArr = htmlArr.filter(el => el.textContent.includes('Flexbox'))
+
+let filtered = htmlArr.map(el => el.dataset.time)
+
+let filteredSeconds = filtered.map(el => {
+    return Number(el.split(':')[0]) * 60 + Number(el.split(':')[1])
+})
+let filteredTotal = (secondsArr => {
+    let num = 0
+    for (let i in secondsArr) {
+        num += secondsArr[i]
+    }
+    return num
+})(filteredSeconds)
+
+
+
+console.log(filteredTotal)
+
+
+//Q2
+
+let numbers = [3,62,234,7,23,74,23,76,92].filter(el => el > 70)
